cba1cd8c4bc424098be16bc2697220a7f595a159
sabaijung <ardsri.s@gmail.com>
2023-02-26 11:20:27 +0700

    เพิ่มไฟล์ การแบ่งเพจ

---
+++ src/helpers/Pagination.js
@@ -0,0 +1,18 @@
+export function pagination(pagesize, currentpage, data) {
+  let value = data
+    .slice(pagesize * (currentpage - 1), pagesize * currentpage)
+    .slice(0, pagesize);
+
+  return {
+    statusCode: 200,
+    taskStatus: true,
+    message: "Success",
+    pagin: {
+      totalrow: data.length,
+      pagesize: pagesize,
+      currentpage: currentpage,
+      totalpage: Math.ceil(data.length / pagesize),
+    },
+    data: value,
+  };
+}
