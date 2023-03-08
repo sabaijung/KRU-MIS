87050e8bea259b3ffed4efab754bb4f332812fb8
sabaijung <ardsri.s@gmail.com>
2023-03-08 11:28:27 +0700

    แก้ไขการเรียกใช้ url

--- src/services/Project.service.js
+++ src/services/Project.service.js
@@ -6,11 +6,24 @@ import Instance from "../helpers/KRU.Axios";
 
 export const getProject = async (pageSize, currentPage, prmProjectName) => {
   try {
-    const response = await Instance.get(
-      `Get/getProject?pagesize=${pageSize}&currentpage=${currentPage}&search=${prmProjectName}`
-    );
+    // const response = await Instance.get(
+    //   `Get/getProject?pagesize=${pageSize}&currentpage=${currentPage}&search=${prmProjectName}`
+    // );
+
+    const response = await Instance.get(`Get/getProject`);
 
     return await response.data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const deleteProject = async (code) => {
+  try {
+    const res = await Instance.delete(
+      `Delete/deleteProject?project_code1=${code}`
+    );
+    return await res.data;
   } catch (error) {
     console.log("error", error);
   }
